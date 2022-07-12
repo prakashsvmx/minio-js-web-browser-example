97ae86c6831af16a610e3afeef1b1255b49e758f
Prakash <prakash@minio.io>
2022-07-12 14:38:56 +0530

    Update to lates and add a Assume role sessiontoken example

--- src/mc.js
+++ src/mc.js
@@ -13,6 +13,20 @@ const mc = new minio.Client(/*{
             accessKey: "minio",
             secretKey: "minio123"
     }
+    //Assume role example::
+    //https://github.com/minio/minio/blob/master/docs/sts/assume-role.md#testing-an-example-with-assume-rolego
+
+    /*,
+    {
+        endPoint: "localhost",
+        useSSL: false,
+        port: 9000,
+        accessKey: "H3ERY005GCGQ98KM0DP5",
+        secretKey: "MSG+WRmt8O8nxeHinNW8v9QcLT1t7JoL14zO71OH",
+        sessionToken: "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NLZXkiOiJIM0VSWTAwNUdDR1E5OEtNMERQNSIsImV4cCI6MTY1NzYyMDA1OSwicGFyZW50IjoibWluaW8ifQ.3ZM7Tg9qMoCV6-sRbmdEc064-Fhtp0eBOBcgetUWx8GfihZZAnInspdiiNZFl_bq43UAh0VHg-etkhkzsvN0EA"
+
+    }*/
+    
 );
 
 
